da396008d2f1b7e0ab156d47bbf831464efdf28b
nekolisha <81178494+nekolisha@users.noreply.github.com>
2021-03-22 10:14:56 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,14 +3,15 @@ namespace SpriteKind {
 }
 tiles.setTilemap(tilemap`level1`)
 let NPC1 = sprites.create(assets.image`clownFish0`, SpriteKind.NPC)
+animation.runImageAnimation(
+NPC1,
+assets.animation`clownFishLeft`,
+100,
+true
+)
 tiles.placeOnRandomTile(NPC1, sprites.builtin.forestTiles0)
 tiles.setTileAt(tiles.getTileLocation(4, 14), assets.tile`transparency16`)
 scene.cameraFollowSprite(NPC1)
 forever(function () {
-    animation.runImageAnimation(
-    NPC1,
-    assets.animation`clownFishLeft`,
-    100,
-    true
-    )
+	
 })
